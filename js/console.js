8f46277c355854980a132eab9503f876a0e05f81
Benjamin Rood <bisr@icloud.com>
2017-12-26 22:10:38 +1300

    Refactor Log.console

--- js/console.js
+++ js/console.js
@@ -1,92 +1,183 @@
-Log = window.Log || {}
+
+let Log = window.Log || {}
 Log.console = {
   history: [],
 
+  startOp: ['start', 'begin'],
+  stopOp: ['stop', 'end', 'pause'],
+  resumeOp: ['resume', 'continue'],
+  addOp: ['add', 'continue'],
+  possibleOperations: [...Log.console.startOp, ...Log.console.stopOp, ...Log.console.resumeOp, ...Log.console.addOp, 'edit', 'delete', 'set', 'import', 'export', 'rename', 'invert'],
+
   /**
-   * Parse command
-   * @param {string} i - Input
+   * @typedef {Object} Command
+   * @property {string} operation - The user-specified command to perform.
+   * @property {string[]} args - Array of string arguments for the operation.
    */
-  parse(i) {
 
-    let command = i.split(' ')[0].toLowerCase()
+  /**
+   * Returns raw user input string into command object.
+   *
+   * Valid user command grammar (EBNF form):
+   *
+   * <command> := <operation> { <arg> }
+   * <operation> := <start> | <stop> | <resume> | <add> | "edit" | "delete" |
+   *                "set" | "import" | "export" | "rename" | "invert"
+   * <start> := "start" | "begin"
+   * <stop> := "stop" | "end" | "pause"
+   * <resume> := "resume" | "continue"
+   * <add> := "add" | "new"
+   * <arg> := <quote> <word> { <word> } <quote>
+   * <quote> := '"'
+   * <word> := <character> { <character> }
+   * <character> := <letter> | <digit> | <symbol>
+   *
+   * Any improperly-formatted commands are discarded and ignored.
+   *
+   * @param {string} str - Raw input from user.
+   * @returns {?Command} If initial word in <str> is valid, else {null}.
+    */
+  parseCmd (str) {
+    const operation = str.split(' ', 1)[0].toLowerCase()
+    if (!Log.console.possibleOperations.includes(operation)) { return } // reject/ignore
 
-    switch (command) {
-      case 'start': case 'begin':
-        Log.console.startLog(i);
-        break;
-      case 'stop': case 'end': case 'pause':
-        Log.console.endLog();
-        break;
-      case 'resume': case 'continue':
-        Log.console.resume();
-        break;
+    let quotedArgs = (str) => {
+      let args = []
+      let quoteStart = false
+      let arg = ''
+      // iterate through chars in string, breaking into quote-wrapped cmd args,
+      // skipping quotes and whitespace between quoted blocks as we go.
+      for (let ch of str) {
+        if (ch === '"') {
+          if (!quoteStart) quoteStart = true  // begin arg wrapped in quotes
+          else {
+            quoteStart = false                // end of arg wrapped in quotes
+            args.push(arg)                    // add argument to array of args
+            arg = ''                          // reset
+          }
+        } else {
+          if (quoteStart) arg += ch           // skip whitespace between args
+        }
+      }
+      return args
+    }
+
+    const args = quotedArgs(str)
+    const command = {operation: operation, args: args}
+    return command
+  },
+
+  /**
+   * Execute the parsed command instruction from the user
+   * @param {Object} cmd - Data structure of parsed user log instruction.
+   * @param {string} cmd.operation - The operation to execute.
+   * @param {string[]} cmd.args - The arguments to be passed to the operation.
+   */
+  executeCmd (cmd) {
+    switch (true) {
+      case cmd.operation in Log.startOp:
+        {
+          const sect = cmd.args[0]
+          const proj = cmd.args[1]
+          const desc = cmd.args[2]
+          Log.console.startLog(sect, proj, desc)
+        }
+        break
+      case cmd.operation in Log.stopOp:
+        Log.console.endLog()
+        break
+      case cmd.operation in Log.resumeOp:
+        Log.console.resume()
+        break
+      case cmd.operation in Log.addOp:
+        {
+          const sect = cmd.args[0]
+          const proj = cmd.args[1]
+          const desc = cmd.args[2]
+          const start = cmd.args[3]
+          const end = cmd.args[4]
+          Log.console.addLog(sect, proj, desc, start, end)
+        }
+        break
       case 'edit':
-        Log.console.edit(i);
-        break;
+        {
+          const id = Number(cmd.args[0]) - 1
+          const attr = cmd.args[1].toLowerCase()
+          const value = cmd.args[2]
+          Log.console.edit(id, attr, value)
+        }
+        break
       case 'delete':
-        Log.console.delete(i);
-        break;
+        {
+          const id = Number(cmd.args[0]) - 1
+          Log.console.delete(id)
+        }
+        break
       case 'set':
-        Log.console.set(i);
-        break;
+        // set ignores all but the first two arguments
+        {
+          const attr = cmd.args[0].toLowerCase()
+          const value = cmd.args[1]
+          Log.console.set(attr, value)
+        }
+        break
       case 'import':
-        Log.console.importUser();
-        break;
+        Log.console.importUser()
+        break
       case 'export':
-        Log.console.exportUser();
-        break;
+        Log.console.exportUser()
+        break
       case 'rename':
-        Log.console.rename(i);
-        break;
+        {
+          const category = cmd.args[0]
+          const oldName = cmd.args[1]
+          const newName = cmd.args[2]
+          Log.console.rename(category, oldName, newName)
+        }
+        break
       case 'invert':
-        Log.console.invert();
-        break;
-      default:
-        return
+        Log.console.invert()
+        break
     }
   },
 
   /**
    * Import user data
    */
-  importUser() {
+  importUser () {
     let path = dialog.showOpenDialog({properties: ['openFile']})
 
-		if (!path) return
+    if (!path) return
 
     let string = ''
-    let notif
 
-		try {
-			string = fs.readFileSync(path[0], 'utf-8')
-		} catch (e) {
-      notif = new window.Notification('An error occured while trying to load this file.')
-		}
+    try {
+      string = fs.readFileSync(path[0], 'utf-8')
+    } catch (e) {
+      window.Notification('An error occured while trying to load this file.')
+    }
 
     localStorage.setItem('user', string)
     user = JSON.parse(localStorage.getItem('user'))
 
-    notif = new window.Notification('Your log data was successfully imported.')
+    window.Notification('Your log data was successfully imported.')
 
-		Log.refresh()
+    Log.refresh()
   },
 
   /**
    * Export user data
    */
-  exportUser() {
+  exportUser () {
     let data = JSON.stringify(JSON.parse(localStorage.getItem('user')))
-    let notif
 
     dialog.showSaveDialog((fileName) => {
       if (fileName === undefined) return
       fs.writeFile(fileName, data, (err) => {
         if (err) {
-          notif = new window.Notification(`An error occured creating the file ${err.message}`)
-
-          return
+          window.Notification(`An error occured creating the file ${err.message}`)
         } else {
-          notif = new window.Notification('Your log data has been exported.')
+          window.Notification('Your log data has been exported.')
         }
       })
     })
@@ -94,52 +185,14 @@ Log.console = {
 
   /**
    * Start a log entry
-   * @param {Object[]} s - Input array
+   * @param {string} sect - Sector label.
+   * @param {string} proj - Project label.
+   * @param {string} desc - Description of activity.
    */
-  startLog(s) {
+  startLog (sect, proj, desc) {
     if (user.log.length !== 0 && user.log.slice(-1)[0].e === 'undefined') return
 
     let start = Log.time.toHex(new Date())
-    let p = []
-    let indices = []
-    let sect = ''
-    let proj = ''
-    let desc = ''
-
-    if (s.includes('"')) {
-      p = s.split('')
-
-      for (let i = 0, l = p.length; i < l; i++) {
-        p[i] === '"' && indices.push(i)
-      }
-
-      for (let i = indices[0] + 1; i < indices[1]; i++) {
-        sect += p[i]
-      }
-
-      for (let i = indices[2] + 1; i < indices[3]; i++) {
-        proj += p[i]
-      }
-
-      for (let i = indices[4] + 1; i < indices[5]; i++) {
-        desc += p[i]
-      }
-    } else if (s.includes(';')) {
-      p = s.split(';')
-      sect = p[0].substring(6, p[0].length).trim()
-      proj = p[1].trim()
-      desc = p[2].trim()
-    } else if (s.includes('|')) {
-      p = s.split('|')
-      sect = p[0].substring(6, p[0].length).trim()
-      proj = p[1].trim()
-      desc = p[2].trim()
-    } else if (s.includes(',')) {
-      p = s.split(',')
-      sect = p[0].substring(6, p[0].length).trim()
-      proj = p[1].trim()
-      desc = p[2].trim()
-    }
 
     user.log.push({
       s: start,
@@ -149,7 +202,29 @@ Log.console = {
       d: desc
     })
 
-    let notif = new window.Notification(`Log started: ${sect} - ${proj} - ${desc}`)
+    window.Notification(`Log started: ${sect} - ${proj} - ${desc}`)
+
+    Log.options.update()
+  },
+
+  /**
+   * Add log entry manually
+   * @param {string} sect - Sector label.
+   * @param {string} proj - Project label.
+   * @param {string} desc - Description of activity.
+   # @param {string} start - Timestamp-formatted string of activity start time.
+   # @param {string} end - Timestamp-formatted string of activity end time.
+   */
+  addLog (sect, proj, desc, start, end) {
+    user.log.push({
+      s: Log.time.convertDateTime(start),
+      e: Log.time.convertDateTime(end),
+      c: sect,
+      t: proj,
+      d: desc
+    })
+
+    window.Notification(`Log added: ${sect} - ${proj} - ${desc}`)
 
     Log.options.update()
   },
@@ -157,117 +232,99 @@ Log.console = {
   /**
    * End a log entry
    */
-  endLog() {
+  endLog () {
     let last = user.log.slice(-1)[0]
     if (last.e !== 'undefined') return
     last.e = Log.time.toHex(new Date())
     clearInterval(timer)
 
-    let notif = new window.Notification(`Log ended: ${last.c} - ${last.t} - ${last.d}`)
+    window.Notification(`Log ended: ${last.c} - ${last.t} - ${last.d}`)
 
     Log.options.update()
   },
 
   /**
    * Resume a paused log entry
+   * @param {Number} id - Row identifier for log entry.
+   *  (default id = -1, i.e. resume the last log entry)
    */
-  resume() {
-    let last = user.log.slice(-1)[0]
+  resume (id = -1) {
+    let entry = user.log.slice(id)[0]
 
-    if (last.e === 'undefined') return
+    if (entry.e === 'undefined') return
 
     user.log.push({
       s: Log.time.toHex(new Date()),
       e: 'undefined',
-      c: last.c,
-      t: last.t,
-      d: last.d
+      c: entry.c,
+      t: entry.t,
+      d: entry.d
     })
 
-    let notif = new window.Notification(`Log resumed: ${last.c} - ${last.t} - ${last.d}`)
+    window.Notification(`Log resumed: ${entry.c} - ${entry.t} - ${entry.d}`)
 
     Log.options.update()
   },
 
   /**
    * Set a config attribute
-   * @param {string} i - Input
+   * @param {string} attr - The config attribute to modify.
+   * @param {string} value - The value the config attribute will be set to.
    */
-  set(i) {
-    let c = i.split(' ')
-    let a = c[1].toLowerCase()
-
-    if (a === 'background' || a === 'bg') {
-      Log.options.setBG(c[2])
-    } else if (a === 'color' || a === 'colour' || a === 'text') {
-      Log.options.setColour(c[2])
-    } else if (a === 'highlight' || a === 'accent') {
-      Log.options.setAccent(c[2])
-    } else if (a === 'font' || a === 'typeface' || a === 'type') {
-      Log.options.setFont(c[2])
-    } else if (a === 'view') {
-      Log.options.setView(c[2])
-    } else if (a === 'cal' || a === 'calendar') {
-      Log.options.setCalendar(c[2])
-    } else if (a === 'timeformat' || a === 'time') {
-      Log.options.setTimeFormat(c[2])
-    } else if (a === 'dateformat' || a === 'date') {
-      Log.options.setDateFormat(c[2])
-    } else if (a === 'weekstart') {
-      Log.options.setWeekStart(c[2])
-    } else if (a === 'category' || a === 'sector' || a === 'cat' || a === 'sec') {
-      Log.options.setColourCode(i)
-    } else if (a === 'project' || a === 'pro') {
-      Log.options.setProjectColourCode(i)
-    } else if (a === 'colourmode' || a === 'colormode') {
-      Log.options.setColourMode(c[2])
-    } else return
+  set (attr, value) {
+    if (attr === 'background' || attr === 'bg') {
+      Log.options.setBG(value)
+    } else if (attr === 'color' || attr === 'colour' || attr === 'text') {
+      Log.options.setColour(value)
+    } else if (attr === 'highlight' || attr === 'accent') {
+      Log.options.setAccent(value)
+    } else if (attr === 'font' || attr === 'typeface' || attr === 'type') {
+      Log.options.setFont(value)
+    } else if (attr === 'view') {
+      Log.options.setView(value)
+    } else if (attr === 'cal' || attr === 'calendar') {
+      Log.options.setCalendar(value)
+    } else if (attr === 'timeformat' || attr === 'time') {
+      Log.options.setTimeFormat(value)
+    } else if (attr === 'dateformat' || attr === 'date') {
+      Log.options.setDateFormat(value)
+    } else if (attr === 'weekstart') {
+      Log.options.setWeekStart(value)
+    } else if (attr === 'category' || attr === 'sector' || attr === 'cat' || attr === 'sec') {
+      Log.options.setColourCode(value)
+    } else if (attr === 'project' || attr === 'pro') {
+      Log.options.setProjectColourCode(value)
+    } else if (attr === 'colourmode' || attr === 'colormode') {
+      Log.options.setColourMode(value)
+    }
   },
 
   /**
    * Delete a log
-   * @param {string} i - Input
+   * @param {Number} id - ID key of log entry to remove from user JSON.
    */
-  delete(i) {
-    user.log.splice(Number(i.split(' ')[1]) - 1, 1)
+  delete (id) {
+    user.log.splice(id - 1, 1)
     Log.options.update()
   },
 
   /**
    * Edit a log
-   * @param {string} i - Input
+   * @param {Number} id - ID key of log entry to modify.
+   * @param {string} attr - The name of the log attribute to modify.
+   * @param {string} value - The value to give the specified log attribute.
    */
-  edit(i) {
-    let c = i.split(' ')
-    let a = c[2]
-    let id = Number(c[1]) - 1
-
-    let proc = input => {
-      let p = input.split('')
-      let indices = []
-      let key = ''
-
-      for (let i = 0, l = p.length; i < l; i++) {
-        p[i] === '"' && indices.push(i)
-      }
-
-      for (let i = indices[0] + 1; i < indices[1]; i++) {
-        key += p[i]
-      }
-
-      return key.trim()
-    }
-
-    if (a === 'sec' || a === 'sector') {
-      user.log[id].c = proc(i)
-    } else if (a === 'title' || a === 'pro' || a === 'project') {
-      user.log[id].t = proc(i)
-    } else if (a === 'desc' || a === 'dsc' || a === 'description') {
-      user.log[id].d = proc(i)
-    } else if (a === 'start') {
-      user.log[id].s = Log.time.convertDateTime(proc(i))
-    } else if (a === 'end') {
-      user.log[id].e = Log.time.convertDateTime(proc(i))
+  edit (id, attr, value) {
+    if (attr === 'sec' || attr === 'sector') {
+      user.log[id].c = value
+    } else if (attr === 'title' || attr === 'pro' || attr === 'project') {
+      user.log[id].t = value
+    } else if (attr === 'desc' || attr === 'dsc' || attr === 'description') {
+      user.log[id].d = value
+    } else if (attr === 'start') {
+      user.log[id].s = Log.time.convertDateTime(value)
+    } else if (attr === 'end') {
+      user.log[id].e = Log.time.convertDateTime(value)
     } else return
 
     Log.options.update()
@@ -275,48 +332,34 @@ Log.console = {
 
   /**
    * Rename a sector or project
-   * @param {Object[]} s - Input
+   * @param {string} category - Either 'sector' or 'project'.
+   * @param {string} oldName - Current label string for sector/project.
+   * @param {string} newName - New label string for sector/project.
    */
-  rename(s) {
-    if (!s.includes('"')) return
+  rename (category, oldName, newName) {
+    // Ignore bad input values
+    if (!(category in ['sector', 'sec', 'project', 'pro'])) return
 
-    let mode = s.split(' ')[1]
-    let p = s.split('')
-    let indices = []
-    let oldName = ''
-    let newName = ''
-    let notif
-
-    let notFound = mode => {
-      let message = mode === 'sector' ? `The sector "${oldName}" does not exist in your logs.` : `The project "${oldName}" does not exist in your logs.`
-      notif = new window.Notification(message)
+    let notFound = category => {
+      let message = category === 'sector' ? `The sector "${oldName}" does not exist in your logs.` : `The project "${oldName}" does not exist in your logs.`
+      window.Notification(message)
     }
 
-    for (let i = 0, l = p.length; i < l; i++) {
-      p[i] === '"' && indices.push(i)
-    }
-
-    for (let i = indices[0] + 1; i < indices[1]; i++) oldName += p[i]
-
-    for (let i = indices[2] + 1; i < indices[3]; i++) newName += p[i]
-
-    if (mode === 'sector' || mode === 'sec') {
+    if (category === 'sector' || category === 'sec') {
       if (Log.data.getEntriesBySector(oldName).length === 0) {
         notFound('sector')
         return
       }
-
       for (let i = 0, l = user.log.length; i < l; i++) {
         if (user.log[i].c === oldName) {
           user.log[i].c = newName
         }
       }
-    } else if (mode === 'project' || mode === 'pro') {
+    } else if (category === 'project' || category === 'pro') {
       if (Log.data.getEntriesByProject(oldName).length === 0) {
         notFound('project')
         return
       }
-
       for (let i = 0, l = user.log.length; i < l; i++) {
         if (user.log[i].t === oldName) {
           user.log[i].t = newName
@@ -324,7 +367,7 @@ Log.console = {
       }
     } else return
 
-    notif = new window.Notification(`The sector "${oldName}" has been renamed to "${newName}."`)
+    window.Notification(`The sector "${oldName}" has been renamed to "${newName}."`)
 
     Log.options.update()
   },
@@ -332,7 +375,7 @@ Log.console = {
   /**
    * Invert interface colours
    */
-  invert() {
+  invert () {
     let bg = user.config.ui.bg
     let c = user.config.ui.colour
 
